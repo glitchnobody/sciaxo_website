3ef5bc2f5735258539d98ae490b24699441aef48
glitchnobody <58382686+glitchnobody@users.noreply.github.com>
2023-11-03 16:20:28 +0530

    Update ESLint config and Next.js settings.

---
+++ src/app/layout.jsx
@@ -0,0 +1,31 @@
+import { Be_Vietnam_Pro, Atkinson_Hyperlegible } from 'next/font/google';
+import './globals.scss';
+
+export const metadata = {
+  title: 'Sciaxo',
+  description: 'sciaxo.com',
+};
+
+const BeVietnam = Be_Vietnam_Pro({
+  weight: ['400', '500'],
+  subsets: ['latin'],
+  style: 'normal',
+  variable: '--heading',
+  display: 'swap',
+});
+
+const Atkinson = Atkinson_Hyperlegible({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  style: 'normal',
+  variable: '--paragraph',
+  display: 'swap',
+});
+
+export default function RootLayout({ children }) {
+  return (
+    <html className={`${BeVietnam.variable} ${Atkinson.variable} `} lang='en'>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import styles from './page.module.scss';
+
+export default function Home() {
+  return (
+    <main>
+      <div className={styles.hehe}>dcd</div>
+    </main>
+  );
+}
