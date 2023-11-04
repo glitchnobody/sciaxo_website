58879bf37f5c18ad8085d7c9bfaecd89f8b23aab
Phantom-d-e-v <atharvashedge@gmail.com>
2023-11-04 20:31:22 +0530

    fonts fix

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,3 @@
-import { Be_Vietnam_Pro, Atkinson_Hyperlegible } from 'next/font/google';
 import './globals.scss';
 import GlobalNav from './GlobalNav';
 
@@ -7,25 +6,22 @@ export const metadata = {
   description: 'sciaxo.com',
 };
 
-const BeVietnam = Be_Vietnam_Pro({
-  weight: ['400', '500'],
-  subsets: ['latin'],
-  style: 'normal',
-  variable: '--heading',
-  display: 'swap',
-});
-
-const Atkinson = Atkinson_Hyperlegible({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-  style: 'normal',
-  variable: '--paragraph',
-  display: 'swap',
-});
-
 export default function RootLayout({ children }) {
   return (
-    <html className={`${BeVietnam.variable} ${Atkinson.variable} `} lang='en'>
+    <html lang='en'>
+      <head>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='true'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible:wght@400;700&family=Be+Vietnam+Pro:wght@400;700&display=swap'
+          rel='stylesheet'
+        />
+      </head>
+
       <body>
         <nav>
           <GlobalNav />
